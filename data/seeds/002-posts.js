dfda969f23ad1ae367430a031471a0fa8fccfa2e
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-19 16:24:18 -0400

    postID typo

--- data/seeds/002-posts.js
+++ data/seeds/002-posts.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('post').insert([
-        {postID: 1, title: 'New York 10/2020', description: "Here is my description of NYC", imageURL: null,  username: 'user1'},
-        {postID: 2, title: 'Chi-Town 9/2020', description: "Here is my description of Chicago", imageURL: null,  username: 'test'},
-        {postID: 3, title: 'Philly 8/2020', description: "Here is my description of Philadelphia!", imageURL: null,  username: 'user2'}
+        {postId: 1, title: 'New York 10/2020', description: "Here is my description of NYC", imageURL: null,  username: 'user1'},
+        {postId: 2, title: 'Chi-Town 9/2020', description: "Here is my description of Chicago", imageURL: null,  username: 'test'},
+        {postId: 3, title: 'Philly 8/2020', description: "Here is my description of Philadelphia!", imageURL: null,  username: 'user2'}
       ]);
     });
 };
