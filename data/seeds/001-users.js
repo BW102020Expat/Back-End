5231c2c0742090fc648e8f4c46cf5900e8cebbe1
mmitch2958 <mikescottmitchell@outlook.com>
2020-10-20 12:36:28 -0400

    updated seed to remove userId for PG

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {userId: 1, username: 'test', password: 'pass123', email: "test@gmail.com"},
-        {userId: 2, username: 'user1', password: 'pass1', email: "user1@gmail.com"},
-        {userId: 3, username: 'user2', password: 'pass2', email: "user2@gmail.com"},
+        { username: 'test', password: 'pass123', email: "test@gmail.com"},
+        { username: 'user1', password: 'pass1', email: "user1@gmail.com"},
+        { username: 'user2', password: 'pass2', email: "user2@gmail.com"},
       ]);
     });
 };
